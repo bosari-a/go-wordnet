13afade411346375fcaea34bf89ef56fe60b8c16
bosari-a <ahmedelbosarimc@gmail.com>
2024-01-10 14:59:17 +0400

    added new regexp for wordnet pos and changed offsets to hash map

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
-	"log"
 	"os"
+	"path"
 	"regexp"
 	"sort"
 	"strings"
@@ -11,43 +12,68 @@ import (
 	"github.com/bosari-a/array-utils/search"
 )
 
-func main() {
-	// the category/type of words defined in array "pos"
-	pos := [4]string{"noun", "verb", "adj", "adv"}
-	// regexp to match a word in a line read from index.pos file
-	wordRegexp := regexp.MustCompile("[a-zA-Z]+")
-	// regexp to match buffer offsets to definitions in data file
-	offsetRegexp := regexp.MustCompile(`\d{8}?`)
+// the category/type of words defined in array "pos"
+var pos [4]string = [4]string{"adv", "adj", "noun", "verb"}
 
-	v, err := os.ReadFile("./dict/index.verb")
-	if err != nil {
-		log.Fatal(err)
+type Word struct {
+	Word        string
+	Definitions map[string]string
+	offsets     map[string][]uint64
+}
+
+func (word *Word) FindOffsets(w string, dictPath string) error {
+	word.offsets = make(map[string][]uint64)
+	for _, ext := range pos {
+		file := fmt.Sprintf("index.%v", ext)
+		filepath := path.Join(dictPath, file)
+		data, err := os.ReadFile(filepath)
+		if err != nil {
+			return err
+		}
+		lines := dataToContent(data)
+		bs := search.BinaryStrFactory(lines, true)
+		// index of the word (-1 if it isn't found)
+		i := bs.BinarySearchStr(w, func(s string) string {
+			return strings.Split(s, " ")[0]
+		})
+		if i != -1 {
+			line := lines[i]
+			// regexp to match corresponding pos
+			posRegexp := regexp.MustCompile(`\s[a-z]{1}?\s`)
+			// corresponding pos is:
+			corrPos := string(posRegexp.Find([]byte(line)))
+			println(corrPos)
+			// regexp to match buffer offsets to definitions in data file
+			offsetRegexp := regexp.MustCompile(`\d{8}?`)
+			offsetsBytes := offsetRegexp.FindAll([]byte(line), -1)
+			offsets := make([]uint64, len(offsetsBytes))
+			for k, v := range offsetsBytes {
+				offsets[k] = binary.BigEndian.Uint64(v)
+			}
+			word.offsets[corrPos] = offsets
+			word.Word = w
+
+		}
 	}
+	return nil
+}
 
-	content := string(v)
+// takes in data from os.ReadFile("index.pos")
+// returns lines, words arrays (sorted)
+func dataToContent(d []byte) []string {
+	content := string(d)
 	lines := strings.Split(content, "\n")
-	sort.Slice(lines, func(i2, j int) bool {
-		return lines[i2] < lines[j]
+	sort.Slice(lines, func(i, j int) bool {
+		return strings.Split(lines[i], " ")[0] < strings.Split(lines[j], " ")[0]
 	})
+	return lines
+}
 
-	var words []string
-	for i := 0; i < len(lines); i++ {
-		line := lines[i]
-		sep := strings.Split(line, " ")
-		words = append(words, sep[0])
+func main() {
+	var word Word
+	err := word.FindOffsets("act", "./dict")
+	if err != nil {
+		panic(err)
 	}
-
-	bs := search.BinaryStrFactory(words, true)
-	i := bs.BinarySearchStr("act")
-	// fmt.Printf("found %v at %v\n", lines[i], i)
-	regWord := regexp.MustCompile("[a-zA-Z]+")
-	found := regWord.Find([]byte(lines[i]))
-
-	regOffsets := regexp.MustCompile(`\d{8}?`)
-	offsetsb := regOffsets.FindAll([]byte(lines[i]), -1)
-	offsets := make([]string, len(offsetsb))
-	for i3, v := range offsetsb {
-		offsets[i3] = string(v)
-	}
-	fmt.Printf("word: %v\noffsets: %v\n", string(found), offsets)
+	fmt.Printf("offsets: %v\n", word.offsets)
 }
